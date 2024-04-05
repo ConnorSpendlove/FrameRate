938c092de95183d51721d2befae170eaf1dcbb11
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-04-04 19:34:47 -0600

    made the main route connect with homepage

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('handlebars');
-const routes = require('./controllers');
+const exphbs = require('express-handlebars'); // Corrected import
+const routes = require('./controllers/api');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
 
 const sess = {
   secret: 'Super secret secret',
@@ -29,7 +27,11 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
+// Create an instance of Express Handlebars
+const hbs = exphbs.create({ defaultLayout: 'main' });
+
+// Set up Handlebars as the template engine
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
@@ -37,6 +39,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+// Default route
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
