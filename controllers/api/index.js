6aebbdf0117b8cc5250c049a0e1af9f362d90a9a
Tyler Sanford <tylersanford1997@gmail.com>
2024-04-09 13:45:32 -0600

    Added routes for login, signup, and profile

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const loginRoutes = require('./loginroutes');
-// const profileRoutes = require('./profileroutes');
+const profileRoutes = require('./profileroutes');
+const signupRoutes = require('./signuproute')
 router.use('/login', loginRoutes);
-// router.use('/profile', profileRoutes);
+router.use('/profile', profileRoutes);
+router.use('/signup', signupRoutes);
+
 module.exports = router;
