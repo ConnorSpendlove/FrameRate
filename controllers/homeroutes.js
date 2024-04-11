dfa55e85c37e447057bd00e1318f07860b89c200
Matt Christopherson <matthew.christopherson27@gmail.com>
2024-04-10 19:34:51 -0500

    .

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('profile', { title: 'Game Browser' });
+  res.render('homepage', { title: 'Game Browser' });
 });
 
-// router.get('/profile', (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (!req.session.logged_in) {
-//     res.redirect('/login');
-//     return;
-//   }
-//   res.render('profile');
-// });
+router.get('/profile', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+  res.render('profile');
+});
 
 router.get('/profile', (req, res) => {
   // If the user is already logged in, redirect the request to another route
